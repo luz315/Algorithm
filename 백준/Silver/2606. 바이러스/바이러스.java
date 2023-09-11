62a2bb59a137ec2a699f44e50fe1258f1ceb1a5d
luz315 <125282732+luz315@users.noreply.github.com>
2023-09-11 12:32:18 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int arr[][];
+	static boolean visit[];
+
+	static int N, M;
+	static int result = 0;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine()); 
+		M = Integer.parseInt(br.readLine()); 
+
+		visit = new boolean[N+1];
+		arr	= new int[N+1][N+1];
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			arr[x][y] = 1;
+            arr[y][x] = 1;
+			
+		}
+
+		DFS(1);
+
+		System.out.println(result);
+	} 
+
+	static void DFS(int node) {
+		visit[node] = true;
+
+		for(int i=1; i<N+1; i++) {
+
+			if(arr[node][i] == 1 && visit[i] == false) {
+				DFS(i);
+				result++;
+			}
+		}
+
+	} 
+} 
