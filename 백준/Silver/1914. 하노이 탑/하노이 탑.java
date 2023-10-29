8869b92662eed7654ebd73c6fec752f2364a5cb2
luz315 <125282732+luz315@users.noreply.github.com>
2023-10-30 03:49:02 +0900

    [Silver I] Title: 하노이 탑, Time: 636 ms, Memory: 79328 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.java
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 public class Main{
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
@@ -11,16 +12,17 @@ public class Main{
         
         if (n <= 20)
             F(n, 1, 2, 3);
+            System.out.println(sb);
     }
     
     public static void F(int n, int A, int B, int C){
         
         if(n==1){
-            System.out.println(A+" "+C);
+            sb.append(A+" "+C).append("\n");
             return;
         }
         F(n-1, A, C, B);
-        System.out.println(A+" "+C);
+        sb.append(A+" "+C).append("\n");
         F(n-1, B, A, C);
         
         return;
