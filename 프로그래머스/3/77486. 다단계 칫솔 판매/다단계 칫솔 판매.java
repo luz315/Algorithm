b24d94d57899566fb3c952aa250b4b161e5f7eed
luz315 <125282732+luz315@users.noreply.github.com>
2023-11-01 15:59:10 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 50.83 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    static Map<String,Integer> map = new HashMap();
+    static Map<String,String> recommand = new HashMap();
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] count = new int[enroll.length];
+        
+        for(int i =0; i<enroll.length;i++){
+            map.put(enroll[i],0);
+            recommand.put(enroll[i],referral[i]);
+        }
+        
+        for(int i=0; i<seller.length; i++){
+            dfs(seller[i],amount[i]*100);
+        }
+        
+        for(int i=0; i<enroll.length;i++){
+            count[i] = map.get(enroll[i]);
+        }
+        
+        return count;
+
+    }
+    
+    public static void dfs(String name, int income){
+        if(name.equals("-") || income==0){
+            return;
+        }
+        
+        int mine =   (int)(Math.ceil(income*0.9));
+        int parents = income - mine;
+        String next = recommand.get(name);
+        map.replace(name,map.get(name)+mine);
+        dfs(next,parents);
+    }
+}
