80b4c8a7e04f6f20c3351229ae428ad2ee263085
luz315 <125282732+luz315@users.noreply.github.com>
2023-11-23 10:50:26 +0900

    [level 2] Title: 오픈채팅방, Time: 98.51 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,25 +1,30 @@
 import java.util.*;
+
 class Solution {
     public List<String> solution(String[] record) {
+       
         Map<String, String> map = new HashMap<>();
         ArrayList<String> answer = new ArrayList<>();
+               
         for(int i = 0; i<record.length; i++){
-                String[] spt=record[i].split(" ");
-            if (!"Leave".equals(spt[0])) {
+            String[] spt =record[i].split(" ");
+            if(spt.length == 3)
                 map.put(spt[1], spt[2]);
-            }
         }
-      for(int i = 0; i<record.length; i++){
-                String[] spt=record[i].split(" ");
-                        switch(spt[0]){
+        
+        for(int i = 0; i<record.length; i++){
+           String[] spt =record[i].split(" ");
+      
+            switch(spt[0]){
                 case "Enter":
                     answer.add(map.get(spt[1])+"님이 들어왔습니다.");
                 break;
+          
                 case "Leave":
                     answer.add(map.get(spt[1])+"님이 나갔습니다.");
                 break;
             }
-        }
+        }                   
         return answer;
     }
 }
