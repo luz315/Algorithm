86ac5b2b96b0fc1998592005bf43bdc51752217c
Poorm <125282732+luz315@users.noreply.github.com>
2024-05-09 02:39:39 +0900

    [Silver IV] Title: 다음 소수, Time: 416 ms, Memory: 12588 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int i =0; i< T; i++){
+            long N = Long.parseLong(br.readLine());
+            if (N <= 1) N = 2;  
+            while(true){
+                int count = 0;
+                for(int j = 2; j<=Math.sqrt(N); j++){
+                    if(N%j==0){
+                        count++;
+                        break;
+                    }
+                }
+                
+                if(count == 0){
+                    System.out.println(N);
+                    break;
+                }
+            N++;
+            }
+        }
+    }
+}
