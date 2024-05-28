302fd96a7adf8bb24601eed4a2ef726079f88eac
Poorm <125282732+luz315@users.noreply.github.com>
2024-05-28 14:46:53 +0900

    [Silver I] Title: 안전 영역, Time: 212 ms, Memory: 15684 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,53 +1,51 @@
 import java.io.*;
 import java.util.*;
 public class Main{
-    static int N;
-    static int limit = 0; 
-    static int dx[] ={0,0,1,-1};
-    static int dy[] ={1,-1,0,0};
-    static int map[][];
+    static int N, arr[][];
+    static int limit = 0;
+    static int result = 0;
     static boolean visit[][];
-    
-    public static void main(String[]args)throws IOException{
+    static int dx[]={0,0,1,-1};
+    static int dy[]={1,-1,0,0};
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
- 
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
+        arr = new int[N][N];
         
-        for(int i = 0; i<N; i++){
+        for(int i =0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<N; j++){
-                map[i][j]=Integer.parseInt(st.nextToken());
-                limit = Math.max(limit,map[i][j]);
+            for(int j=0; j<N;j++){
+                arr[i][j]=Integer.parseInt(st.nextToken());
+                limit = Math.max(limit,arr[i][j]);
             }
         }
-        int result = 0;
-        for(int h = 0; h<=limit; h++){
+        for(int d = 0; d<limit;d++){
             visit = new boolean[N][N];
             int count = 0;
-            for(int i =0; i<N; i++){
-                for(int j=0; j<N; j++){
-                    if(!visit[i][j] && map[i][j]>h){
-                        dfs(i, j, h);
+            for(int i=0;i<N;i++){
+                for(int j=0; j<N;j++){
+                    if(!visit[i][j]&&arr[i][j]>d){
                         count++;
+                        back(i,j,d);
                     }
                 }
             }
-            result = Math.max(result,count);
+            result = Math.max(count, result);
         }
         System.out.println(result);
-        
     }
-    public static void dfs(int x, int y, int height){
-        visit[x][y] = true;
-        
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(0<=nx&&nx<N && 0<=ny&&ny<N && map[nx][ny]>height && !visit[nx][ny]){
-                dfs(nx, ny, height);
-            }   
-        }
+    
+    public static void back(int x, int y, int depth){
+        visit[x][y]=true;
+        for(int i=0; i<4;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(0<=nx&&nx<N && 0<=ny&&ny<N && !visit[nx][ny] && arr[nx][ny]>depth){
+                
+                back(nx, ny, depth);
+                
+            }
+        }        
     }
 }
