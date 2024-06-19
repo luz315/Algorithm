bb7e69fbf0710e043a563a5f824301aee5551e38
Poorm <125282732+luz315@users.noreply.github.com>
2024-06-20 08:41:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1692 ms, Memory: 83840 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,47 +1,47 @@
 import java.io.*;
 import java.util.*;
 public class Main{
-    static int x,y,N;
+    static ArrayList<Integer> arr[];
+    static int N,a,b,parent[];
     static boolean visit[];
-    static int parent[];
-    static ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-      
-        for(int i=0; i<=N;i++){
-            arr.add(new ArrayList<>());
-        }
-        
-        for(int i =0; i<N-1; i++){
-            st= new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            arr.get(x).add(y);
-            arr.get(y).add(x);
-        }
         visit = new boolean[N+1];
         parent = new int[N+1];
+        arr =new ArrayList[N+1];
         
-        DFS(1);
-        
-        for(int i = 2;i<parent.length; i++){
-            System.out.println(parent[i]);
-            
+        for(int i=1; i<=N; i++){
+            arr[i] = new ArrayList<>();
         }
-    
+        
+        for(int i=1; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            a=Integer.parseInt(st.nextToken());
+            b=Integer.parseInt(st.nextToken());
+            arr[a].add(b);
+            arr[b].add(a);   
+        }
+        bfs(1);
+        
+        for(int i=2; i<=N; i++){
+            System.out.println(parent[i]);
+        }
     }
-    
-    public static void DFS(int a){
-        visit[a] = true;
-       
-         for (int i : arr.get(a)) {
-            if (!visit[i]) {
-                DFS(i);
-                parent[i]=a;
+    static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visit[start]=true;
+        
+        while(!q.isEmpty()){
+            int p = q.poll();
+            for(int next:arr[p]){
+                if(!visit[next]){
+                    visit[next]=true;
+                    q.add(next);
+                    parent[next]=p;
+                }
             }
         }
-             
     }
 }
