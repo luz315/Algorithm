b105afa25763d8b8c9605059a8d5d79dfcdfff7d
Poorm <125282732+luz315@users.noreply.github.com>
2024-06-25 08:04:28 +0900

    [Silver II] Title: 부분수열의 합, Time: 96 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,42 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
- 
-public class Main {
- 
-	static int N, S;
-	static int arr[], result[];
-	static int cnt;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		dfs(0, 0);
-		if(S == 0) {
-			System.out.println(cnt-1);
-		}else {
-			System.out.println(cnt);
-		}
-	}
-	
-	private static void dfs(int index, int sum) {
-		if(index == N) {
-			if(sum == S) {
-				cnt++;
-			}
-			return;
-		}
-		dfs(index+1, sum+arr[index]);
-		dfs(index+1, sum);
-	}
+import java.io.*;
+import java.util.*;
+public class Main{
+    static int N,S,arr[];
+    static int count=0;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i]= Integer.parseInt(st.nextToken());
+        }
+        
+        
+        dfs(0,0);
+        
+        if(S==0){
+            System.out.println(count-1);
+        }
+        else{
+            System.out.println(count);
+        }
+    }
+    static void dfs(int start, int sum){
+        if(start==N){
+            if(sum==S){
+                count++;
+            }
+            return;
+        }
+        
+        int next = start+1;
+        
+        dfs(next,sum+arr[start]);
+        dfs(next,sum);
+    }
 }
