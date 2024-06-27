f337f57dddbd26ed125881996a33c17c3db63e99
Poorm <125282732+luz315@users.noreply.github.com>
2024-06-27 17:55:12 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 76 ms, Memory: 13260 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,60 +1,57 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 public class Main{
-    static int N;
-    static int arr[];
-    static int cal[] = new int[4];
-    static int max = Integer.MIN_VALUE;    
+    static int N, arr[], cal[];
+    static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
+    static int result =0;
+        
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        arr=new int[N];
+        cal=new int[4];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++){
-            arr[i] = Integer.parseInt(st.nextToken());        
+        for(int i=0;i<4;i++){
+            cal[i]=Integer.parseInt(st.nextToken());
         }
-         st = new StringTokenizer(br.readLine());
-        for(int j=0;j<4;j++){
-            cal[j] = Integer.parseInt(st.nextToken());
-        }
-        Back(arr[0],1);
+        
+        
+        brute(arr[0],1);
+        
         System.out.println(max);
         System.out.println(min);
     }
     
-    public static void Back(int step, int index){
-        if(index == N){
-            max = Math.max(max,step);
-            min = Math.min(min,step);
+    static void brute(int result, int depth){
+        if(depth==N){
+            max = Math.max(result, max);
+            min = Math.min(result, min);
             return;
         }
+        
         for(int i=0; i<4; i++){
             if(cal[i]>0){
                 cal[i]--;
-            
+                
                 switch(i){
-                    case 0:
-                        Back(step + arr[index], index+1);
-                        break;
-                 
-                    case 1:
-                        Back(step - arr[index], index+1);
-                        break;
-                        
-                    case 2:
-                        Back(step * arr[index], index+1);
-                        break;
                     
-                    case 3:
-                        Back(step / arr[index], index+1);
+                    case 0: brute(result+arr[depth], depth+1);
+                        break;
+                    case 1: brute(result-arr[depth], depth+1);
+                        break;
+                    case 2: brute(result*arr[depth], depth+1);
+                        break;
+                    case 3: brute(result/arr[depth], depth+1);
                         break;
                 }
                 cal[i]++;
             }
-       
         }
-       
+        
     }
 }
