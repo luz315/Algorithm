4a10307e2644b088c7aa1ca28c8f0671e04426a4
Poorm <125282732+luz315@users.noreply.github.com>
2024-06-29 00:51:13 +0900

    [Silver IV] Title: 설탕 배달, Time: 64 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,34 +1,24 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class Main { 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int dp[] = new int[N + 1];
+        int INF = Integer.MAX_VALUE / 2;
 
-	public static void main(String[] args) throws Exception {
-		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N + 1];
-        
-		if (N >= 3)
-			dp[3] = 1;
-		if (N >= 5)
-			dp[5] = 1;
-		
+        Arrays.fill(dp, INF);
+
+        if (N >= 3) dp[3] = 1;
+        if (N >= 5) dp[5] = 1;
+
         for (int i = 6; i <= N; i++) {
-			if (i % 5 == 0) {
-				dp[i] = dp[i - 5] + 1;
-			} else if (i % 3 == 0) {
-				dp[i] = dp[i - 3] + 1;
-			} else {
-				if (dp[i - 3] != 0 && dp[i - 5] != 0) {
-					dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
-				}
-			}
-		}
-		
-        if (dp[N] == 0) {
-			System.out.println("-1");
-			return;
-		}
-		System.out.println(dp[N]);
-	}
+            if (dp[i - 3] != INF || dp[i - 5] != INF) {  // 유효한 값을 가지고 있을 때만 갱신
+                dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
+            }
+        }
+
+        System.out.println(dp[N] == INF ? -1 : dp[N]);
+    }
 }
