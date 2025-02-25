b95f9226baef21eb3ee5660f476893304f2b6e4c
Poorm <125282732+luz315@users.noreply.github.com>
2025-02-25 10:53:32 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 11816 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,53 +1,45 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
-    static int arr[], N, M;
+public class Main{
+    static int N,M,arr[];
     static boolean visit[];
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[101];
         visit = new boolean[101];
-
-        for (int i = 0; i < N + M; i++) {
+        for(int i=0;i<N+M;i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             arr[x] = y;
         }
-
-        bfs(1);
+        
+        bfs(1); 
     }
-
-    static void bfs(int start) {
+    static void bfs(int start){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{start, 0});
-        visit[start] = true;
-
+        q.add(new int[]{start,0});
+        visit[start]=true;
+        
         while (!q.isEmpty()) {
-            int[] p = q.poll();
-
-            if (p[0] == 100) {
+            int p[] = q.poll();
+            if(p[0]==100){
                 System.out.println(p[1]);
                 return;
             }
-
-            for (int i = 1; i <= 6; i++) {
-                int next = p[0] + i;
-                if (next <= 100) {
-                    if (!visit[next]) {
-                        visit[next] = true;
-                        if (arr[next] != 0) {
-                            q.add(new int[]{arr[next], p[1] + 1});
-                        } else {
-                            q.add(new int[]{next, p[1] + 1});
-                        }
-                    }
-                }
+            for(int i=1;i<=6;i++){
+               int n = p[0]+i;
+               if(n<=100 && !visit[n]){
+                   visit[n]=true;
+                   if(arr[n]!=0){
+                       q.add(new int[]{arr[n],p[1]+1});
+                   }else{
+                       q.add(new int[]{n,p[1]+1});
+                   }
+               }
             }
         }
     }
