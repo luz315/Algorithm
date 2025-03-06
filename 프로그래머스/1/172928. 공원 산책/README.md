a33bcb1d3d4a63f304b05ec44106bc650aea8a8b
Poorm <125282732+luz315@users.noreply.github.com>
2025-03-06 15:52:19 +0900

    [level 1] Title: 공원 산책, Time: 0.68 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
 
 ### 성능 요약
 
-메모리: 74.8 MB, 시간: 0.31 ms
+메모리: 86.8 MB, 시간: 0.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 28일 15:40:03
+2025년 03월 06일 15:52:17
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -4,49 +4,41 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] park, String[] routes) {
         char arr[][] = new char[park.length][park[0].length()];
-        int result[] =new int[2];
-        int sx = 0;
-        int sy = 0;
+        int start[]= new int[2];
         
-        for(int i=0;i<park.length; i++){
+        for(int i=0;i<park.length;i++){
             arr[i]=park[i].toCharArray();
-            for(int j=0; j<park[0].length();j++){
+            for(int j=0;j<park[0].length();j++){
                 if(arr[i][j]=='S'){
-                     sx=i;
-                     sy=j;
+                    start[0]=i;
+                    start[1]=j;
                 }
             }
         }
         
-        
-        for(String route : routes){
-            String dir = route.split(" ")[0];
-            int move = Integer.parseInt(route.split(" ")[1]);
-            int nx = sx;
-            int ny = sy;
-            
-            for(int j=0; j<move; j++){
-                switch(dir){
+        for(String r:routes){
+            String[] s = r.split(" ");
+            int num = Integer.parseInt(s[1]); 
+            int nx = start[0];
+            int ny = start[1];
+            for(int i=0;i<num;i++){
+                switch(s[0]){
                     case "E": ny++; break;
+                    case "S": nx++; break;
                     case "W": ny--; break;
                     case "N": nx--; break;
-                    case "S": nx++; break;
                 }
-                 if (nx < 0 || ny >= park[0].length() || ny < 0 || nx >= park.length || arr[nx][ny] == 'X') {
-                    nx = sx; 
-                    ny = sy;
+                
+                if (nx < 0 || nx >= park.length || ny < 0 || ny >= park[0].length() || arr[nx][ny] == 'X') {
                     break;
                 }
                 
-                else if (j == move - 1) {
-                    sx = nx;
-                    sy = ny;
+                 if (i == num - 1) {
+                    start[0] = nx;
+                    start[1] = ny;
                 }
             }
-            
         }
-        result[0] = sx; 
-        result[1] = sy; 
-        return result;
+        return start;
     }
 }
