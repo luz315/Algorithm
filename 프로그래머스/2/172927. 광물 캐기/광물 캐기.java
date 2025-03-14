7e45bd300e7eb003f5f3fb981dd4d7160a2b6c1e
Poorm <125282732+luz315@users.noreply.github.com>
2025-03-14 11:02:01 +0900

    [level 2] Title: 광물 캐기, Time: 316.16 ms, Memory: 287 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,49 +1,42 @@
 import java.util.*;
-
 class Solution {
-    
     static int result = Integer.MAX_VALUE;
-    
-    public int solution(int[] picks, String[] minerals)     {
-        
+    public int solution(int[] picks, String[] minerals) {
         dfs(picks, minerals, 0, 0);
         return result;
     }
-         
     
-    public static void dfs(int[] picks, String[] minerals, int depth, int stress){
-        if(depth == minerals.length || Arrays.stream(picks).sum()==0){
+    static void dfs(int picks[], String minerals[], int depth, int stress){
+        if(depth==minerals.length || Arrays.stream(picks).sum()==0){
             result = Math.min(result,stress);
             return;
         }
-        
-        for(int i=0;i<3; i++){
+        for(int i=0;i<3;i++){
             if(picks[i]>0){
                 picks[i]--;
-                int sum = stress;
-                for(int j=depth; j<Math.min(depth+5, minerals.length);j++){
+                int count = stress;
+                for(int j=depth; j<Math.min(depth+5, minerals.length); j++){
                     switch(i){
                         case 0:
-                            sum +=1;
+                            count++;
                             break;
                         case 1:
                             if(minerals[j].equals("diamond"))
-                                sum+=5;
+                                count+=5;
                             else
-                                sum+=1;
+                                count++;
                             break;
                         case 2:
                             if(minerals[j].equals("diamond"))
-                                sum+=25;
+                                count+=25;
                             else if(minerals[j].equals("iron"))
-                                    sum+=5;
+                                count+=5;
                             else
-                                sum+=1;
+                                count++;
                             break;
-                            
                     }
                 }
-                dfs(picks, minerals, depth+5, sum);
+                dfs(picks, minerals, depth+5, count);
                 picks[i]++;
             }
         }
