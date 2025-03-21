fb18ae885110501125c352a0a8175bfdda40d091
Poorm <125282732+luz315@users.noreply.github.com>
2025-03-21 22:59:37 +0900

    [level 3] Title: 단속카메라, Time: 8.52 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,17 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (o1, o2)->{
-            return o1[1] - o2[1];
-        });
-        int count =1;
-        int now = routes[0][1];
-        for(int i=1;i<routes.length;i++){
-            if(routes[i][0]>now){
-                now = routes[i][1];
-                count++;
-            }
-        }
-        return count;
+        int answer = 0;
+        return answer;
     }
 }
